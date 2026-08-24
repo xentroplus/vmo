e82aa2f3aaa64df8a382d7a70f29e3bca0e69e74
xentroplus <miandradew@gmail.com>
2026-08-24 12:41:49 -0500

    v1.2 - Corrige logo Coach Ismael y vista previa al compartir

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = 'vmo-pwa-v1.1';
+const CACHE = 'vmo-pwa-v1.2';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./brand.png',
-  './coach-logo.png'];
+  './coach-logo.png',
+  './social-preview-v1.2.jpg'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(r => { const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r; }).catch(()=>caches.match('./index.html')))); });
